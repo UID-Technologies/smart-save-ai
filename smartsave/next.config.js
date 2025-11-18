31f08c72091c0d14565deb7351b4281be5329d32
Varun Gupta <thevarungupta1@gmail.com>
2025-11-18 10:09:57 +0530

    updated

--- smartsave/next.config.js
+++ smartsave/next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: [],
   },
+  // Required for Docker: Next.js must listen on 0.0.0.0 to accept external connections
+  experimental: {
+    serverActions: true,
+  },
 }
 
 module.exports = nextConfig
